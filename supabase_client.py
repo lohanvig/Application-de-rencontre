cdf7c23b98e56601b39a5581132bfd10d89c6416
lohan vignals <lohan.vignals@iut-rodez.fr>
2026-03-11 08:44:35 +0100

    Suppression test voitr si connexion existante

--- supabase_client.py
+++ supabase_client.py
@@ -4,18 +4,3 @@ SUPABASE_URL = "https://nnwgpojusfppkdrtcmil.supabase.co"
 SUPABASE_KEY = "sb_publishable_kKkCK9DelZEQrGRZVXiqdQ_ODgYoADL"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-data = {
-    "username": "alex",
-    "email": "alex@test.com",
-    "age": 25,
-    "bio": "Aime voyager"
-}
-
-response = supabase.table("users").insert(data).execute()
-
-print(response)
-
-response = supabase.table("users").select("*").execute()
-
-print(response)
