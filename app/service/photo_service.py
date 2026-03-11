218700c12ee39fed81e1f9d3c8a0fbd9cfda1be5
lohan vignals <lohan.vignals@iut-rodez.fr>
2026-03-11 10:07:17 +0100

    Génération nom unique pour la photo

--- app/service/photo_service.py
+++ app/service/photo_service.py
@@ -1,21 +1,34 @@
-from app.database.supabase_client import supabase
+def upload_photo(user_id, file_path, original_filename):
+    import uuid
+    from app.database.supabase_client import supabase
 
-SUPABASE_URL = "https://nnwgpojusfppkdrtcmil.supabase.co"
+    SUPABASE_URL = "https://nnwgpojusfppkdrtcmil.supabase.co"
 
-def upload_photo(user_id, file_path, filename):
+    # générer un nom unique
+    ext = original_filename.split(".")[-1]
+    filename = f"{user_id}_{uuid.uuid4()}.{ext}"
 
+    # upload fichier
     with open(file_path, "rb") as f:
         supabase.storage.from_("profile-photos").upload(
             filename,
             f
         )
 
+    # URL publique
     url = f"{SUPABASE_URL}/storage/v1/object/public/profile-photos/{filename}"
 
-    supabase.table("photos").insert({
+    # insérer dans la table photos
+    response = supabase.table("photos").insert({
         "user_id": user_id,
         "photo_url": url,
         "is_main": True
     }).execute()
+
+    # vérifier si l'insertion a renvoyé des données
+    if not response.data:
+        print("Erreur insertion photo : aucune donnée insérée")
+    else:
+        print("Photo ajoutée dans la table photos :", url)
 
     return url
